00a08f3b163532a194d2a9895dc6bf1a4004b920
acsnorbika <acsnorbert@turr.hu>
2025-12-08 11:51:08 +0100

    Backend feltöltése, app.ts fixálás

--- SzallasFoglalo/src/app/app.ts
+++ SzallasFoglalo/src/app/app.ts
@@ -1,13 +1,13 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LandingComponent } from "../components/system/landing-page/landing-page.component";
+
 import { FooterComponent } from "../components/system/footer/footer.component";
 import { NavbarComponent } from "../components/system/navbar/navbar.component";
-import { HeaderComponent } from '../components/system/header/header.component';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, LandingComponent, FooterComponent, HeaderComponent, NavbarComponent],
+  imports: [RouterOutlet,  FooterComponent, NavbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
