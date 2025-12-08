40e60a26d30119aa53cc5bf91a8dd45550bc4a9a
acsnorbika <acsnorbert@turr.hu>
2025-12-08 10:29:25 +0100

    Landing page kész

--- SzallasFoglalo/src/app/app.ts
+++ SzallasFoglalo/src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LandingComponent } from "../components/system/landing-page/landing-page.component";
+import { FooterComponent } from "../components/system/footer/footer.component";
+import { HeaderComponent } from "../components/system/header/header.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LandingComponent, FooterComponent, HeaderComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
