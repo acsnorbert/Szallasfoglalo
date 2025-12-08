ac038bfc38331744f176eedeadd91cc4a356f2d9
JelencsityMiklous <jelencsitymiklos@turr.hu>
2025-12-08 10:30:04 +0100

    Profil-Regisztráció-Bejelentkezés oldal elkészítése

--- SzallasFoglalo/src/components/user/profile/profile.component.ts
+++ SzallasFoglalo/src/components/user/profile/profile.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-profile',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, RouterModule],
   templateUrl: './profile.component.html',
-  styleUrl: './profile.component.scss'
+  styleUrls: ['./profile.component.scss']
 })
 export class ProfileComponent {
+  user = {
+    name: 'John Doe',
+    currentPassword: '',
+    newPassword: '',
+    confirmPassword: ''
+  };
 
+  updateProfile() {
+    if (!this.user.name) {
+      alert('A felhasználónév nem lehet üres!');
+      return;
+    }
+
+    if (this.user.newPassword || this.user.confirmPassword) {
+      if (this.user.newPassword !== this.user.confirmPassword) {
+        alert('Az új jelszó és a megerősítés nem egyezik!');
+        return;
+      }
+
+      if (!this.user.currentPassword) {
+        alert('Add meg a jelenlegi jelszavad a változtatáshoz!');
+        return;
+      }
+    }
+
+    console.log('Profil frissítve:', this.user);
+    alert('Sikeres profilfrissítés!');
+
+  }
 }
--- SzallasFoglalo/src/components/user/registration/registration.component.ts
+++ SzallasFoglalo/src/components/user/registration/registration.component.ts
@@ -1,12 +1,43 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, RouterModule],
   templateUrl: './registration.component.html',
-  styleUrl: './registration.component.scss'
+  styleUrls: ['./registration.component.scss']
 })
 export class RegistrationComponent {
+  newUser = {
+    name: '',
+    email: '',
+    password: '',
+    confirm: '',
+    phone: '',
+    address: ''
+  };
 
+  acceptTerms: boolean = false;
+
+  registration() {
+    if (!this.newUser.name || !this.newUser.email || !this.newUser.password || !this.newUser.confirm) {
+      alert('Kérlek, töltsd ki a kötelező mezőket!');
+      return;
+    }
+
+    if (this.newUser.password !== this.newUser.confirm) {
+      alert('A jelszavak nem egyeznek!');
+      return;
+    }
+
+    if (!this.acceptTerms) {
+      alert('El kell fogadnod a regisztrációs szabályzatot!');
+      return;
+    }
+
+    console.log('Regisztráció:', this.newUser);
+    alert(`Sikeres regisztráció: ${this.newUser.name}`);
+  }
 }
