42ffe265f8a2708160aac05d4f65d75875c7a504
JelencsityMiklous <jelencsitymiklos@turr.hu>
2025-12-08 10:00:31 +0100

    Komponensek létrehozása

---
+++ SzallasFoglalo/src/components/system/accommodation-list/accommodation-list.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accommodation-list',
+  standalone: true,
+  imports: [],
+  templateUrl: './accommodation-list.component.html',
+  styleUrl: './accommodation-list.component.scss'
+})
+export class AccommodationListComponent {
+
+}
---
+++ SzallasFoglalo/src/components/user/mybookings/mybookings.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-mybookings',
+  standalone: true,
+  imports: [],
+  templateUrl: './mybookings.component.html',
+  styleUrl: './mybookings.component.scss'
+})
+export class MybookingsComponent {
+
+}
